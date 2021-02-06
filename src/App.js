320d2d0de0bf483a5646608edefb3ecf0d076c5c
idhant96 <idhant123@gmail.com>
2021-02-06 15:31:32 -0700

    job cards working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import useFetchJobs from './useFetchJobs';
+import {Container} from 'react-bootstrap';
+import Jobs from './Jobs';
+
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const {jobs, loading, error} = useFetchJobs();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <h1>New Jobs!</h1>
+      {loading && <h1>loading...</h1>}
+      {error && <h1>error ...</h1>}
+      {jobs.map(job => {
+        return <Jobs key={job.id} job={job} />
+      })}
+    </Container>
   );
 }
 
 export default App;
+
